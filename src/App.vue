dbb13e0bee3b689d027a771163584701ac9b6ef4
simoneeth <simone.bucciol@monogrid.com>
2022-06-24 14:42:35 +0200

    add map + setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import { defineComponent} from 'vue'
+import Map from './atoms/Map.vue'
+export default defineComponent({
+    name: "App",
+    components: { Map }
+})
+</script>
+
+<template>
+<Map />
+</template>
+
+<style scoped>
+@font-face {
+  font-family: MonsterratThin;
+  src: url("./assets/fonts/Monsterrat/Montserrat-Thin.ttf");
+}
+@font-face {
+  font-family: MonsterratRegular;
+  src: url("./assets/fonts/Monsterrat/Montserrat-Regular.ttf");
+}
+@font-face {
+  font-family: MonsterratBold;
+  src: url("./assets/fonts/Monsterrat/Montserrat-Bold.ttf");
+}
+@font-face {
+  font-family: MonsterratBlack;
+  src: url("./assets/fonts/Monsterrat/Montserrat-Black.ttf");
+}
+
+html{
+  scroll-behavior: smooth;
+  background: black;
+  font-family: MonsterratRegular;
+}
+</style>
