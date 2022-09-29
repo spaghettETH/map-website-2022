5ba95531e940e68bbc0db89f101ae1eb9909ead5
simoneeth <bucciolsimone@gmail.com>
2022-09-29 14:27:22 +0200

    style: fix map overflow

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -183,7 +183,7 @@ export default defineComponent({
         class="w-full h-full flex items-center justify-center z-100 absolute"
       >
         <ul
-          v-if="communitiesToDisplay.length !== 0 && miniMap"
+         v-if="communitiesToDisplay.length !== 0 && miniMap"
           class="
             communities-wrapper
             relaitve
@@ -246,10 +246,10 @@ export default defineComponent({
         </ul>
         <div
           v-if="communitiesToDisplay.length === 0 && miniMap"
-          class="no-communities-wrapper w-full"
+          class="no-communities-wrapper"
         >
           <svg
-            class="w-full h-full"
+          class="w-full h-full"
             version="1.1"
             id="Livello_1"
             xmlns="http://www.w3.org/2000/svg"
@@ -265,16 +265,14 @@ export default defineComponent({
               stroke-width="0.5"
               d="M60.9 74.2c0-5.8-6.7-10.6-14.9-10.6-2.2 0-4.3.4-6.2 1-1.8-3.3-5.9-5.5-10.6-5.5-5 0-9.3 2.5-10.9 6.1h-.8C8 65.2.2 69.6.2 75c0 5 6.6 9.1 15.1 9.7 1 7.1 9.3 12.7 19.4 12.7 10.8 0 19.5-6.3 19.5-14.1v-.2c4.1-2 6.7-5.2 6.7-8.9z"
             />
-            <path
-              class="st1"
-              fill="#ff9c23"
+            <path class="st1"
+            fill="#ff9c23"
               stroke="#000000"
               stroke-width="0.75"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-miterlimit="10"
-              d="m90.7 104-2.1 40.4 12.1 7.9 20.3-6.5V102z"
-            />
+            d="m90.7 104-2.1 40.4 12.1 7.9 20.3-6.5V102z" />
             <path
               class="st2"
               fill="#7474f4"
@@ -307,26 +305,21 @@ export default defineComponent({
             <path
               class="st4"
               fill="#00f0c1"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-miterlimit="10"
               d="M68.6 73.4s-20.4 34-17.4 38.7c2.9 4.7 11.2 19.4 30.9-4.2L68.6 73.4zM144.3 73.4s20.4 34 17.4 38.7c-2.9 4.7-11.2 19.4-30.9-4.2l13.5-34.5z"
             />
-            <ellipse
-              class="st4"
-              fill="#00f0c1"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-miterlimit="10"
-              cx="105.8"
-              cy="80.7"
-              rx="39.7"
-              ry="36"
-            />
+            <ellipse class="st4"
+            fill="#00f0c1"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-miterlimit="10"
+            cx="105.8" cy="80.7" rx="39.7" ry="36" />
             <path
               class="st1"
               fill="#ff9c23"
@@ -380,46 +373,44 @@ export default defineComponent({
             <path
               class="st5"
               fill="#ffffff"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
               d="M76.1 74.1s-3.6-5.3.7-10.5 12.5-2.9 12.5-2.9 4.9 2 4.3 7.7-4.6 6.7-4.6 6.7-8.6 3.3-12.9-1z"
             />
             <path
               class="st6"
               fill="#00cfff"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
               d="M71.1 79.3s1.5-5.6 8.6-5.7c7-.1 2.9.6 6.1 1.1s5.2 1.2 5 3.6c-.2 2.4-2.5 2.6-5.1 2.1s-6.5.1-7.1.8c-.6.7-3.1.7-3.1.7s-2.4 3.3-4.3 1-.1-3.6-.1-3.6z"
             />
             <path
               class="st5"
               fill="#ffffff"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
               d="M118.3 70s-1.5-6.3 4.3-8.8c5.8-2.5 11.5.4 12.4 2 .8 1.7 3.1 8 1.3 10.8-1.9 2.9-16.6 3.8-18-4z"
             />
+            <path class="st7"
+            fill="none"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
+            d="M97.4 86.7c3.8-5.5 14.6-5 16.8 0" />
             <path
               class="st7"
               fill="none"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
-              d="M97.4 86.7c3.8-5.5 14.6-5 16.8 0"
-            />
-            <path
-              class="st7"
-              fill="none"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
               d="M102.6 83.8c0 .5-.4.9-.9.9s-.9-.4-.9-.9M110.8 83.8c0 .5-.4.9-.9.9s-.9-.4-.9-.9"
             />
             <circle cx="84.9" cy="67.4" r=".8" />
@@ -447,10 +438,10 @@ export default defineComponent({
             <path
               class="st6"
               fill="#00cfff"
-              stroke="#000000"
-              stroke-width="0.75"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
+                stroke="#000000"
+                stroke-width="0.75"
+                stroke-linecap="round"
+                stroke-miterlimit="10"
               d="M121.2 77.5s.5-2.5 5.9-2.9c0 0 8-4.4 13.1 2.9 5.1 7.3-2.1 5.7-4.5 4.5-2.4-1.2-5-1.8-5-1.8s-3.3 1.1-7.1.1c-3.8-.9-2.4-2.8-2.4-2.8z"
             />
             <path
@@ -461,9 +452,7 @@ export default defineComponent({
               d="M184.6 131.2c-.6 0-1.2 0-1.8.1-1.2-1.2-3.2-2.2-5.6-2.8.3-.7.4-1.4.4-2.1 0-3-2.4-5.4-5.4-5.4-3 0-5.4 2.4-5.4 5.4 0 .7.1 1.4.4 2-4.5 1-7.6 3.3-7.6 6v.3c-.4-.2-.9-.3-1.4-.3-2.3 0-4.1 1.9-4.1 4.1 0 2.3 1.9 4.1 4.1 4.1 2.3 0 4.1-1.9 4.1-4.1v-.1c1.7 1.1 3.9 1.8 6.6 2.2.9 2.9 3.6 4.9 6.8 4.9 3.9 0 7.1-3.2 7.1-7.1v-.1c.6.1 1.2.1 1.9.1 5 0 9-1.6 9-3.6s-4.1-3.6-9.1-3.6zM138.5 147.4c-1 0-2 .2-2.9.6-2.6-6.7-9.2-11.5-16.8-11.5-3 0-5.9.8-8.4 2.1-1.8-.9-3.8-1.4-6-1.4-3.7 0-7 1.5-9.5 3.8-4.4-5.1-10.9-8.3-18.2-8.3-5.9 0-11.2 2.1-15.4 5.6-2.3-3-6.1-5.1-10.6-5.5-.3-4.1-3.8-7.4-8-7.4-4.4 0-8 3.6-8 8 0 2 .8 3.9 2 5.3-1.3 1.8-2 3.8-2 6.1 0 6.6 6.4 12 14.4 12 1.2 0 2.4-.1 3.5-.4v.4c0 13.3 10.7 24 24 24C82 180.8 87 179 91 176c2.2 2.9 5.7 4.8 9.7 4.8 5.7 0 10.5-3.9 11.8-9.2 1.9.7 4 1.1 6.2 1.1 7.8 0 14.4-4.9 16.9-11.9.8.4 1.8.6 2.7.6 3.9 0 7-3.1 7-7 .2-3.9-2.9-7-6.8-7z"
             />
           </svg>
-          <h2 class="p-4 text-center">
-            Non ci sono ancora communities in questa regione!
-          </h2>
+          <h2 class="p-4">Non ci sono ancora communities in questa regione!</h2>
           <button>Registra la tua!</button>
         </div>
       </div>
@@ -3387,21 +3376,20 @@ section {
   }
 }
 
-.no-communities-wrapper {
+.no-communities-wrapper{
   background-color: #343536;
   height: 50%;
-  max-width: 40%;
   padding: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  border-radius: 10px;
+  border-radius: 15px;
 
-  @media(max-width:640px){
-    max-width: 95%;
-    min-width: 90%;
+  @media(max-width: 640px){
+    max-width: 90%;
   }
+
 
   & h2 {
     font-size: 1.4rem;
@@ -3410,26 +3398,26 @@ section {
   }
 
   & button {
-    background-color: #f99bc0;
-    color: #f2f2f2;
-    padding: 1rem;
-    font-weight: bold;
-    border-radius: 1rem;
-    transition: opacity 0.2s ease-in-out;
+     background-color: #f99bc0;
+  color: #f2f2f2;
+  padding: 1rem;
+  font-weight: bold;
+  border-radius: 1rem;
+  transition: opacity .2s ease-in-out;
 
-    @media (max-width: 640px) {
-      padding: 0.7rem;
-      font-size: 0.8em;
-    }
+  @media(max-width:640px){
+    padding: .7rem;
+    font-size: .8em;
+  }
 
-    &:hover {
-      opacity: 0.8;
-    }
+  &:hover {
+    opacity: .8;
+  }
   }
 }
 .map {
   @media (max-width: 640px) {
-    min-width: 95%;
+    width: 90%;
   }
 }
 
