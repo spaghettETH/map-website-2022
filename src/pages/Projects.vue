0adc4b105fc3c71e3e33aa8eb91eba3c21cda7ab
Matlemad <matteo.tambussi@gmail.com>
2025-10-03 13:30:11 +0200

    added Zine3 link

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -39,7 +39,7 @@ export default defineComponent({
         links: [
           { text: 'Zine#1', url: 'https://spaghetteth.mypinata.cloud/ipfs/bafybeigho7543eqdd5yl3zhfo3cjvzwdpwdylaafalgncffwcbposoh2py' },
           { text: 'Zine#2', url: 'https://spaghetteth.mypinata.cloud/ipfs/bafybeiaux26nuvahpq3vpbwhtitsu4v7vspcm4srlz2horumgvd26hatae' },
-          { text: 'Candida un articolo per la Zine #3', url: 'https://docs.google.com/forms/d/e/1FAIpQLSeUzcmpyOv5D03wGZoJsWIrba9HlgO3MDD7cDSotT2BTnFwFw/viewform?usp=preview' }
+          { text: 'Zine #3', url: 'https://spaghetteth.mypinata.cloud/ipfs/bafybeidv5dxfjh2g7yf7u62eu5lsujzejzbvtg6srilqwgdeg6i27b64bq' }
         ],
         tags: ['Publishing', 'Community', 'Education']
       },
